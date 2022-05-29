40533cbb81b9018223decbdbcaceb0ea88836e46
maliukh <noirkotyara@gmail.com>
2022-05-29 18:24:22 +0300

    update mail options

--- index.js
+++ index.js
@@ -21,41 +21,25 @@ exports.handler = (event, context, callback) => {
         }
     })
 
-    const done = (err, res) => callback(null, {
-        statusCode: err ? '400' : '200',
-        body: err ? err.message : JSON.stringify(res),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST'
-        },
-    });
-
-    let _body = JSON.parse(event.body);
-
-    let body_txt='<h2>Contact Form Details</h2>';
-    for (const key in _body) {
-        const res = key.replace("_", " ");
-        body_txt +='<p><strong>' + titleCase(res) +' : </strong>'+_body[key] + '</p>';
-    }
-
-
-    let mailOptions = {
-        from: 'noirkotyara@gmail.com',
-        to: 'noirkotyara@gmail.com',
-        subject: 'SUBJECT',
-        html: body_txt
+    const done = (err, res) => {
+        return res.send({
+            statusCode: err ? '400' : '200',
+            body: err ? err.message : JSON.stringify(res),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST'
+            },
+        })
     };
 
-    transporter.sendMail(mailOptions, done(null,_body));
-};
+    let mailOptions =  {
+        from: 'noirkotyara@gmail.com', // sender address
+        to: 'noirkotyara@gmail.com', // list of receivers
+        subject: 'Hello', // Subject line
+        text: 'Hello world', // plaintext body
+        html: '<b>Hello world</b>' // html body
+    };
 
-// convert the key as the label
-function titleCase(str) {
-    const splitStr = str.toLowerCase().split(' ');
-    for (let i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    // return the joined string
-    return splitStr.join(' ');
-}
+    transporter.sendMail(mailOptions, done);
+};
