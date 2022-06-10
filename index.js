4b1a67d31ab32bdfb9e5da225312e966c28d0262
maliukh <noirkotyara@gmail.com>
2022-06-10 09:04:33 +0300

    export in module

--- index.js
+++ index.js
@@ -10,38 +10,43 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-exports.handler = (event, context, callback) => {
-    console.log('LOGS createTransport', {
-        host: process.env.SMTP_HOST,
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    })
+// exports.handler = ;
 
-    console.log('LOGS EVENT', event)
-    console.log('LOGS CONTEXT', context)
-    console.log('LOGS CALLBACK', callback)
+module.exports = {
+    handler: (event, context, callback) => {
+        console.log('LOGS createTransport TEST', {
+            host: process.env.SMTP_HOST,
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        })
 
-    const done = () => callback(null, {
-        statusCode: err ? '400' : '200',
-        body: err ? err.message : 'No errorssss',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST'
-        },
-    });
+        console.log('LOGS EVENT', event)
+        console.log('LOGS CONTEXT', context)
+        console.log('LOGS CALLBACK', callback)
 
-    let mailOptions =  {
-        from: 'noirkotyara@gmail.com', // sender address
-        to: 'noirkotyara@gmail.com', // list of receivers
-        subject: 'Hello', // Subject line
-        text: 'Hello world', // plaintext body
-        html: '<b>Hello world</b>' // html body
-    };
+        const done = () => callback(null, {
+            statusCode: err ? '400' : '200',
+            body: err ? err.message : 'No errorssss',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST'
+            },
+        });
 
-    transporter.sendMail(mailOptions, done);
-};
+        let mailOptions =  {
+            from: 'noirkotyara@gmail.com', // sender address
+            to: 'noirkotyara@gmail.com', // list of receivers
+            subject: 'Hello', // Subject line
+            text: 'Hello world', // plaintext body
+            html: '<b>Hello world</b>' // html body
+        };
+
+        transporter.sendMail(mailOptions, done);
+    }
+
+}
